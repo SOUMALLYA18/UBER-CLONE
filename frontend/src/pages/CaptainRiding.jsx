c844b89961ab11e95d1371d1a0c3f0b73c965692
SOUMALLYA MUKHERJEE <soumallya.india@gmail.com>
2025-01-27 23:00:02 +0530

    bug fix

--- frontend/src/pages/CaptainRiding.jsx
+++ frontend/src/pages/CaptainRiding.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import uberLogo from "../../public/images/uber-driver.svg";
 import { Link, useLocation } from "react-router-dom";
-import FinishRide from "../components/Finishride";
+import FinishRide from "../components/FinishRide";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import LiveTracking from "../components/LiveTracking";
